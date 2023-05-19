0a1d7a2ac13eaef8bcd5d4e359118898059b2c1c
SreedharLakshman2 <sreedharlakshman2@gmail.com>
2023-05-19 13:35:26 +0530

    flip_coin UI updated.

--- lib/flip_coin.dart
+++ lib/flip_coin.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
-class FlipCoin extends StatelessWidget {
+class FlipCoin extends StatefulWidget {
+  const FlipCoin({super.key});
+
+  @override
+  State<FlipCoin> createState() => FlipCoinState();
+}
+
+class FlipCoinState extends State<FlipCoin> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 1),
+    reverseDuration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.elasticOut,
+  );
+
+  final choiceArray = ["heads", "tails", "heads", "tails", "tails", "heads"];
+  bool showAnimate = false;
+  String flipResult = 'heads';
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +48,65 @@ class FlipCoin extends StatelessWidget {
         child: Column(
           children: [
             Spacer(),
-            Image.asset('assets/tail.png'),
+            showAnimate
+                ? RotationTransition(
+                    turns: _animation,
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Image.asset('assets/head.png'),
+                    ),
+                  )
+                : Image.asset(
+                    flipResult == 'heads'
+                        ? 'assets/head.png'
+                        : 'assets/tail.png',
+                    width: 150,
+                    height: 150,
+                  ),
+            SizedBox(
+              height: 50,
+            ),
+            !showAnimate
+                ? ElevatedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        showAnimate = true;
+                      });
+                      Future.delayed(const Duration(seconds: 3), () {
+// Here you can write your code
+                        setState(() {
+                          final random = new Random();
+                          this.flipResult =
+                              choiceArray[random.nextInt(choiceArray.length)];
+                          showAnimate = false;
+                          ScaffoldMessenger.of(context).clearSnackBars();
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              backgroundColor:
+                                  Color.fromARGB(255, 166, 23, 174),
+                              content: Text(
+                                flipResult == 'heads'
+                                    ? 'It' 's heads'
+                                    : 'It' 's tails',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              duration: const Duration(seconds: 2),
+                            ),
+                          );
+                        });
+                      });
+                    },
+                    icon: Icon(Icons.flip), //icon data for elevated button
+                    label: Text(
+                      'Flip a coin',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ), //label text
+                  )
+                : Spacer(),
             Spacer(),
           ],
         ),
